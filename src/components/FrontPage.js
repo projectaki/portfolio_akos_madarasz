1c69477dd75af8cef130bb14459bc820c6bf15e5
projectaki <akosegypro@gmail.com>
2021-02-18 18:25:06 +0100

    margin fix

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -12,19 +12,20 @@ class FrontPage extends Component {
                     </div>
                     
                 </div>
-                <div class="row" style={{position: "absolute", top: "65%", left: "98%", transform: "translate(-65%, -98%)", width: "40vmin"}} >
+                <div class="row" style={{position: "absolute", top: "65%", left: "93%", transform: "translate(-65%, -93%)", display: "flex"}} >
 
-                                <div className="col s1 " style={{fontSize: "2vmin", paddingLeft: "", paddingRight: "6vmin"}}>
+                                <div className="col s2 " style={{fontSize: "2vmin", paddingLeft: 0, paddingRight: 0, width: "6vmin", marginRight: "2vmin"}}>
                                     <a href="https://github.com/projectaki" style={{"color":"#9a43a7"}}>
                                         <i className="fab fa-github fa-3x"  ></i>
                                     </a>
                                 </div>
-                                <div className="col s1 " style={{fontSize: "2vmin", paddingLeft: "", paddingRight: ""}}>
-                                    <a href="https://www.linkedin.com/in/akos-madarasz-b430441a1/" style={{"color":"#0a66c2"}} >
+                                <div className="col s2 " style={{fontSize: "2vmin", paddingLeft: 0, paddingRight: 0, width: "6vmin"}}>
+                                    <center><a href="https://www.linkedin.com/in/akos-madarasz-b430441a1/" style={{"color":"#0a66c2"}} >
                                         <i className="fab fa-linkedin fa-3x" ></i>
-                                    </a>
+                                    </a></center>
+                                    
                                 </div>
-                                <div className="col s1 " style={{fontSize: "2vmin", paddingLeft: "6vmin", paddingRight: ""}}>
+                                <div className="col s2 " style={{fontSize: "2vmin", paddingLeft: 0, paddingRight: 0, width: "6vmin", marginLeft: "2vmin"}}>
                                     <a href="mailto:akosegypro@gmail.com" style={{"color":"#0f9d58"}}>
                                         <i className="fab fa-google fa-3x "></i>
                                     </a>
