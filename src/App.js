58df95b5f0b5eb22d6336d5109a0267a693f696d
projectaki <akosegypro@gmail.com>
2021-02-28 12:32:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,38 +4,25 @@ import 'materialize-css/dist/js/materialize.min.js';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import Profile from './components/Profile';
 import Projects from './components/Projects';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import CarouselComponent from './components/CarouselComponent';
-import Test from "./components/Test";
 import FrontPage from "./components/FrontPage";
-import Footer from './components/Footer';
 
 
 function App() {
   return (
-    <>
-    
     <HashRouter basename="/">
-    
-    
-      <>
-        <Route exact path = "/" component={FrontPage}/>
+
         <Switch>
-          
-          <Route path="/Projects" component = {Projects}/>
-          <Route path="/Profile" component = {Profile}/>
-          <Route path="/CarouselComponent" component = {CarouselComponent}/>
-          <Route path="/test" component = {Test}/>
+          <Route exact path = "/" component={FrontPage}/>
+          <Route exact path= "/Projects" component = {Projects}/>
+          <Route exact path= "/Profile" component = {Profile}/>
         </Switch>
-      </>
-      
+
     </HashRouter>
     
 
 
 
-    </>
+    
   );
 }
 
